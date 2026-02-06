1d5f1cab8a0bceee7ef0b647d588405e203d1938
Mahi Singh <mahisingh@Mahis-MacBook-Air.local>
2026-02-07 01:19:44 +0530

    Fix App routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
-import History from './pages/History';
+import History from './pages/History'; // Make sure this import is here!
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/scan" element={<Home />} />
-        <Route path="/history" element={<History />} />
-      </Routes>
-    </Router>
+    <Routes>
+      {/* 1. The Landing Page (Login) */}
+      <Route path="/" element={<Login />} />
+      
+      {/* 2. The Signup Page */}
+      <Route path="/signup" element={<Signup />} />
+      
+      {/* 3. The Main Scanner Page (This was missing!) */}
+      <Route path="/home" element={<Home />} />
+      
+      {/* 4. The History Page (This was missing!) */}
+      <Route path="/history" element={<History />} />
+    </Routes>
   );
 }
 
